2fb67db0d8795f5e83693e8e566735c3fefb2fd3
Syd7 <sydpaguio@gmail.com>
2024-04-24 23:32:12 +0800

    Wrote Draft of GameFrame and GameStarter
    refer to comments on GameFrame, I believe it will give us direction on how to do GameCanvas aswell.

--- GameFrame.java
+++ GameFrame.java
@@ -1,7 +1,28 @@
-//This class contains the code that sets up the Main JFrame for the player.
+//This class contains the code that sets up the Main JFrame for the player. 
 
-public class GameFrame {
-    public GameFrame() {
+//since its just rough lang naman i am basing off the video sir provided! more specifically 
+//(Supplement) Video List: Sending Coordinates Between 2 Players Over a Network in Java
+// Part 1 - 9:06
+// REFER TO 10 MINUTES ONWARDS FOR HOW TO DO GAME CANVAS I BELIEVE.
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    private int width, height;
+    private Container contentPane;
+
+    public GameFrame(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    public void setUpGUI() {
+        contentPane = this.getContentPane();
+        this.setTitle("Wild Whisker Shootout");
+        contentPane.setPreferredSize(new Dimension(width, height));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack(); // idk what this does no lie
+        this.setVisible(true);
 
     }
 }
--- GameStarter.java
+++ GameStarter.java
@@ -2,5 +2,7 @@
 //side.
 public class GameStarter {
     public static void main(String[] args) {
+        GameFrame gf = new GameFrame(800, 600); // arbitrary
+        gf.setUpGUI();
     }
 }
