fe8757d1b232e2d6500e180bbd99b7219c95e023
JPChurros <151139142+JPChurros@users.noreply.github.com>
2024-04-28 14:33:10 +0800

    Platform (NOT COMPLETE)

---
+++ Platform.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Platform implements DrawingObject {
+    public Platform(){
+
+    }
+    public void draw(Graphics2D g2d){}
+
+    public void adjustX(double distance){}
+
+    public void animateX(double multiplier){}
+
+    public double getX(){
+        return 0;
+    }
+}
